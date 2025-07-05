7d5cf309f699a6ddc9c64c0caa4fb7742e1f0cf9
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2025-07-05 17:15:25 +0700

    chore: Remove unused Header component from Index page for cleaner code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,5 @@
 
 import React, { useEffect } from 'react';
-import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
@@ -19,7 +18,6 @@ const Index = () => {
 
   return (
     <div className="min-h-screen bg-background">
-      <Header />
       <main>
         <Hero />
         <About />
