4a0186edf7b49df010803a900ea1a249b5e451c0
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2025-09-05 15:44:51 +0700

    feat: Define ImportMetaEnv interface for blog admin credentials

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BLOG_ADMIN_EMAIL: string
+  readonly VITE_BLOG_ADMIN_PASSWORD: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
